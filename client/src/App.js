93e4ffa50f9e8674e8bdc9904ca54e7c0ee73024
Georg Kirschner <gk@bridgerbowl.com>
2017-10-18 15:54:11 -0600

    add basic render beer

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      beers: []
+    }
+  }
+
+  componentWillMount() {
+    axios.get('/api/beers')
+      .then((response) => {
+        this.setState({
+          beers: response.data
+        })
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    let beerItems = this.state.beers.map( (beer) => {
+      return <li key={ beer.id }>{ beer.name }</li>
+    });
+
     return (
       <div>
         <header>
           <h1>Georgie's Favorite Beers</h1>
         </header>
-        <ul></ul>
+        <ul> { beerItems } </ul>
       </div>
     );
   }
