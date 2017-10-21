2296422277ee20a2cb62f7184584ab3043d90567
Georg Kirschner <gk@bridgerbowl.com>
2017-10-20 20:13:15 -0600

    removing solution for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import BeerList from './BeerList';
-import BeerDetails from './BeerDetails';
-import BeerUpdate from './BeerUpdate';
 
 class App extends Component {
 
   render() {
-    return (
-			<BrowserRouter>
-				<Switch>
-					<Route exact path='/' component={BeerList} />
-					<Route exact path='/beers/:id' component={BeerDetails} />
-					<Route exact path='/beers/update/:id' component={BeerUpdate} />
-				</Switch>
-			</BrowserRouter>
-    );
+    return <div></div>;
   }
 }
 
