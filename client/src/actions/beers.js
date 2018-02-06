6b2e4dfd628c6c096a7beefc7ea6c17b1af37806
Georg Kirschner <gk@bridgerbowl.com>
2018-02-06 14:27:49 -0700

    action variables for fetch

--- client/src/actions/beers.js
+++ client/src/actions/beers.js
@@ -2,19 +2,22 @@ import axios from 'axios';
 
 let nextBeerId = 0;
 
+export const FETCH_BEERS_PENDING = 'FETCH_BEERS_PENDING';
+export const FETCH_BEERS_FULFILLED = 'FETCH_BEERS_FULFILLED';
+export const FETCH_BEERS_REJECTED = 'FETCH_BEERS_REJECTED';
 export const ADD_BEER = 'ADD_BEER';
 export const DELETE_BEER = 'DELETE_BEER';
 export const UPDATE_BEER = 'UPDATE_BEER';
 
 export function fetchBeers() {
   return function(dispatch) {
-    dispatch( {type:'FETCH_BEERS_PENDING'} );
+    dispatch( {type: FETCH_BEERS_PENDING} );
 		axios.get('/api/beers')
 			.then( (response) => {
-				dispatch( {type:'FETCH_BEERS_FULFILLED', payload:response.data} );
+				dispatch( {type: FETCH_BEERS_FULFILLED, payload:response.data} );
 			})
 			.catch( (err) => {
-				dispatch( {type:'FETCH_BEERS_REJECTED', payload:err} );
+				dispatch( {type:FETCH_BEERS_REJECTED, payload:err} );
       })
     }
 }
