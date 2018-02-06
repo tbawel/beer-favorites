93d1459260a3acea9c553431ed568c413493ee34
Georg Kirschner <gk@bridgerbowl.com>
2018-02-06 15:24:28 -0700

    add redux middleware

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { logger } from "redux-logger";
+import promise from "redux-promise-middleware";
 import { Provider } from 'react-redux';
 import reducers from './reducers/';
 import App from './App';
@@ -9,7 +10,7 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
-const middleware = applyMiddleware(logger);
+const middleware = applyMiddleware(promise(), logger);
 const store = createStore(reducers, middleware);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
