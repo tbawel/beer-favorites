9a890de4bc5701b7d83e17ecf9e3452fb5620a52
Georg Kirschner <gk@bridgerbowl.com>
2018-02-06 14:20:56 -0700

    fetchingn beers action

--- client/src/actions/beers.js
+++ client/src/actions/beers.js
@@ -1,8 +1,23 @@
+import axios from 'axios';
+
 let nextBeerId = 0;
 
 export const ADD_BEER = 'ADD_BEER';
 export const DELETE_BEER = 'DELETE_BEER';
 export const UPDATE_BEER = 'UPDATE_BEER';
+
+export function fetchBeers() {
+  return function(dispatch) {
+    dispatch( {type:'FETCH_BEERS_PENDING'} );
+		axios.get('/api/beers')
+			.then( (response) => {
+				dispatch( {type:'FETCH_BEERS_FULFILLED', payload:response.data} );
+			})
+			.catch( (err) => {
+				dispatch( {type:'FETCH_BEERS_REJECTED', payload:err} );
+      })
+    }
+}
 
 export function addBeer(beer) {
   return {
