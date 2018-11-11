b3348542e42691085647e92f2853a5591b991ba2
Georgie Kirschner <georgiekirschner@Georgies-MacBook-Pro.local>
2018-11-11 10:54:40 -0700

    delete beer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Button
+} from "reactstrap";
 import "./App.css";
 
 class App extends Component {
@@ -8,27 +15,50 @@ class App extends Component {
     this.state = {
       beers: [
         {
+          id: 0,
           name: "Choco Stout",
           alcoholContent: 2.4,
           brewery: "Bridger Brewing"
         },
         {
+          id: 1,
           name: "Blonde White",
           alcoholContent: 3.1,
           brewery: "406 Brewing"
         },
         {
+          id: 2,
           name: "Thunder Monkey",
           alcoholContent: 4.2,
           brewery: "MAP Brewing"
         }
       ]
     };
+
+    this.deleteBeer = this.deleteBeer.bind(this);
+  }
+
+  deleteBeer(beerId) {
+    let newBeers = this.state.beers.filter(beer => beer.id !== beerId);
+
+    this.setState({
+      beers: newBeers
+    });
   }
 
   render() {
     let beerFavs = this.state.beers.map(beer => {
-      return <ListGroupItem>{beer.name}</ListGroupItem>;
+      return (
+        <ListGroupItem key={beer.id}>
+          {beer.name}
+          <Button
+            color="danger float-right"
+            onClick={this.deleteBeer.bind(this, beer.id)}
+          >
+            Delete
+          </Button>
+        </ListGroupItem>
+      );
     });
 
     return (
