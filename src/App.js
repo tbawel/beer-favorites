ff41521830e3d698a100be872e81646ee0d888ff
Georgie Kirschner <georgiekirschner@Georgies-MacBook-Pro.local>
2018-11-11 10:45:40 -0700

    rendering static beers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      beers: [
+        {
+          name: "Choco Stout",
+          alcoholContent: 2.4,
+          brewery: "Bridger Brewing"
+        },
+        {
+          name: "Blonde White",
+          alcoholContent: 3.1,
+          brewery: "406 Brewing"
+        },
+        {
+          name: "Thunder Monkey",
+          alcoholContent: 4.2,
+          brewery: "MAP Brewing"
+        }
+      ]
+    };
+  }
+
   render() {
+    let beerFavs = this.state.beers.map(beer => {
+      return <ListGroupItem>{beer.name}</ListGroupItem>;
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Beer Favorites</h1>
+              <ListGroup>{beerFavs}</ListGroup>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
