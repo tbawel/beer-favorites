c839d6a2670b341a471f19486149a19ecec156d8
Georgie Kirschner <georgiekirschner@Georgies-MacBook-Pro.local>
2018-11-11 11:10:38 -0700

    creating new beer form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import {
   Col,
   ListGroup,
   ListGroupItem,
-  Button
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
 } from "reactstrap";
 import "./App.css";
 
@@ -32,15 +36,40 @@ class App extends Component {
           alcoholContent: 4.2,
           brewery: "MAP Brewing"
         }
-      ]
+      ],
+      newBeerName: "",
+      newBeerAlc: 0,
+      newBeerBrewery: ""
     };
 
     this.deleteBeer = this.deleteBeer.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   deleteBeer(beerId) {
     let newBeers = this.state.beers.filter(beer => beer.id !== beerId);
 
+    this.setState({
+      beers: newBeers
+    });
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    let newBeer = {
+      name: this.state.newBeerName,
+      alcoholContent: this.newBeerAlc,
+      brewery: this.newBeerBrewery
+    };
+
+    let newBeers = this.state.beers.slice();
+    newBeers.push(newBeer);
     this.setState({
       beers: newBeers
     });
@@ -52,7 +81,8 @@ class App extends Component {
         <ListGroupItem key={beer.id}>
           {beer.name}
           <Button
-            color="danger float-right"
+            color="danger"
+            className="float-right"
             onClick={this.deleteBeer.bind(this, beer.id)}
           >
             Delete
@@ -68,6 +98,61 @@ class App extends Component {
             <Col>
               <h1>Beer Favorites</h1>
               <ListGroup>{beerFavs}</ListGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <h3>Create New Beer</h3>
+              <Form onSubmit={this.handleSubmit}>
+                <FormGroup>
+                  <Row>
+                    <Col sm="3">
+                      <Label for="newBeerName">Name:</Label>
+                    </Col>
+                    <Col sm="9">
+                      <Input
+                        type="text"
+                        name="newBeerName"
+                        id="newBeerName"
+                        onChange={this.handleChange}
+                      />
+                    </Col>
+                  </Row>
+                </FormGroup>
+                <FormGroup>
+                  <Row>
+                    <Col sm="3">
+                      <Label for="newBeerAlc">Alcohol Content:</Label>
+                    </Col>
+                    <Col sm="9">
+                      <Input
+                        type="text"
+                        name="newBeerAlc"
+                        id="newBeerAlc"
+                        onChange={this.handleChange}
+                      />
+                    </Col>
+                  </Row>
+                </FormGroup>
+                <FormGroup>
+                  <Row>
+                    <Col sm="3">
+                      <Label for="newBeerBrewery">Brewery:</Label>
+                    </Col>
+                    <Col sm="9">
+                      <Input
+                        type="text"
+                        name="newBeerBrewery"
+                        id="newBeerBrewery"
+                        onChange={this.handleChange}
+                      />
+                    </Col>
+                  </Row>
+                </FormGroup>
+                <Button type="submit" className="float-right">
+                  Create Beer
+                </Button>
+              </Form>
             </Col>
           </Row>
         </Container>
